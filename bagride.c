842b2f07fcd1053abefb3dcc44c5079732e06b3d
= <=>
2013-11-29 16:45:40 +0100

    ok udp send

--- bagride.c
+++ bagride.c
@@ -99,19 +99,6 @@ void br_udp_server_register(br_udp_server_t* udp_servers_, int size_) {
     }
 }
 
-void br_udp_clientKO_register(br_udp_client_t* udp_clients_, int size_) {
-    int i = 0;
-    for (i = 0; i < size_; i++) {
-        br_udp_client_t* client = &udp_clients_[i];     
-        MM_INFO("(%d) udp sending on %s:%d", i, client->m_addr, client->m_port);
-        uv_udp_init(uv_default_loop(), &client->m_handler);
-        
-        client->m_handler.data = client;
-        MM_ASSERT(0 == uv_ip4_addr(client->m_addr, client->m_port, &client->m_socketaddr));
-        MM_ASSERT(0 == uv_udp_bind(&client->m_handler, (const struct sockaddr*) &client->m_socketaddr, 0));      
-    }
-}
-
 int br_udp_client_register(br_udp_client_t* cli_) {
     uv_udp_init(uv_default_loop(), &cli_->m_handler);
     if (0 > uv_ip4_addr(cli_->m_addr, cli_->m_port, &cli_->m_socketaddr)) return -1;
--- mss_filter.c
+++ mss_filter.c
@@ -7,7 +7,6 @@
 #include "mmtrace.h"
 
 
-
 #define HELP_USAGE "usage: mss_filter addr port" 
 #define UDP_AIS_PORT 9998
 #define HTTP_AIS_PORT 9997
@@ -26,57 +25,55 @@ static char forward_sentence[1024] = {0};
 
 br_udp_client_t udp_client;
 
-
-
-static void on_send(uv_udp_send_t* req_, int status){
-  (void)status;
-  char *base = (char*) req_->data;
-  free(base);
-  free(req_);
+static void on_send(uv_udp_send_t* req_, int status) {
+    (void) status;
+    char *base = (char*) req_->data;
+    free(base);
+    free(req_);
 }
 
-static int on_ais_decoded(struct sad_filter_s * filter_){
-    
+static int on_ais_decoded(struct sad_filter_s * filter_) {
+
     struct ais_t * ais = &filter_->ais;
     sub0_substring_t* sentence = filter_->sentence;
-    
-    if(1 == ais->type || 2 == ais->type  || 3 == ais->type ){
-        
-        if(0 == strncmp(last_sentence, sentence->start, sentence->n)){
+
+    if (1 == ais->type || 2 == ais->type || 3 == ais->type) {
+
+        if (0 == strncmp(last_sentence, sentence->start, sentence->n)) {
             /* drop duplicate */
 #ifdef DEBUG            
-            printf("[KO] drop duplicate %08" PRIu64 " type:%02d mmsi:%09u %s", 
-            filter_->sentences, 
-            ais->type, 
-            ais->mmsi,
-            filter_->sentence->start);
+            printf("[KO] drop duplicate %08" PRIu64 " type:%02d mmsi:%09u %s",
+                    filter_->sentences,
+                    ais->type,
+                    ais->mmsi,
+                    filter_->sentence->start);
 #endif            
             return 0;
         }
-        
+
         strncpy(last_sentence, sentence->start, sentence->n);
-        last_sentence[sentence->n + 1] = '0'; 
-        
-        const double lat = (double)ais->type1.lat / AIS_LATLON_DIV;
-        const double lon = (double)ais->type1.lon / AIS_LATLON_DIV;
-        
-        if(lon > x1 && lon < x2 && lat < y1 && lat > y2){
-	  
-	  strncpy(forward_sentence, sentence->start, sentence->n);
-	  forward_sentence[sentence->n] = '\n';
-	  forward_sentence[sentence->n + 1] = '\0';
-	  
-            printf("[ok] %08" PRIu64 " type:%02d mmsi:%09u lat:%f lon:%f %s", 
-            filter_->sentences, 
-            ais->type, 
-            ais->mmsi,
-            lat,
-            lon,
-            forward_sentence);
-            
+        last_sentence[sentence->n + 1] = '0';
+
+        const double lat = (double) ais->type1.lat / AIS_LATLON_DIV;
+        const double lon = (double) ais->type1.lon / AIS_LATLON_DIV;
+
+        if (lon > x1 && lon < x2 && lat < y1 && lat > y2) {
+
+            strncpy(forward_sentence, sentence->start, sentence->n);
+            forward_sentence[sentence->n] = '\n';
+            forward_sentence[sentence->n + 1] = '\0';
+
+            printf("[ok] %08" PRIu64 " type:%02d mmsi:%09u lat:%f lon:%f %s",
+                    filter_->sentences,
+                    ais->type,
+                    ais->mmsi,
+                    lat,
+                    lon,
+                    forward_sentence);
+
             br_udp_client_send(&udp_client, forward_sentence);
-	    
-	}
+
+        }
 
     }
     return 0;
@@ -108,31 +105,31 @@ br_http_server_t http_servers[] = {
     },
 };
 
-static void mss_info_error(void){
-  printf(HELP_USAGE "\n");
+static void mss_info_error(void) {
+    printf(HELP_USAGE "\n");
 }
 
-int main(int argc, char **argv) { 
+int main(int argc, char **argv) {
 
     if (3 != argc) goto err;
-    
+
     udp_client.m_port = atoi(argv[2]);
     udp_client.m_addr = argv[1];
-    
-    MM_INFO("init %s:%d", udp_client.m_addr, udp_client.m_port);  
-    
+
+    MM_INFO("init %s:%d", udp_client.m_addr, udp_client.m_port);
+
     if (sad_filter_init(&filter, on_ais_decoded, NULL)) goto err;
-    
+
     br_udp_server_register(udp_servers, sizeof (udp_servers) / sizeof (br_udp_server_t));
     br_http_server_register(http_servers, sizeof (http_servers) / sizeof (br_http_server_t));
-    
-    if( 0 > br_udp_client_register(&udp_client)) goto err;
-    
+
+    if (0 > br_udp_client_register(&udp_client)) goto err;
+
     br_run();
 
     return 0;
-    
- err:   
+
+err:
     mss_info_error();
     return 1;
 
