74dd980e644896f10e0a7e2ecd6a72252452e74e
manuel-m <discoride.dev@gmail.com>
2014-01-02 20:24:22 +0100

    test pool infra

---
+++ tests/test_pool.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+
+
+
+#include "mmpool.h"
+
+static int has_failed = 0;
+
+int main(int argc, char **argv) {
+    (void) argv;
+    (void) argc;
+
+    mmpool_t* pool = NULL;
+    
+    (void)pool;
+
+
+    return has_failed;
+}
+
