0bcaa18270f55ef3c52477b7580281782a641917
manuel-m <discoride.dev@gmail.com>
2014-01-13 20:53:54 +0100

    scalar:mmgen_config.lua

--- mmgen_config.lua
+++ mmgen_config.lua
@@ -55,6 +55,97 @@ function mmgen_struct(rootnode, structname)
     io.write("};\n")
 end
 
+function mmgen_affectnodes(node, nh, level)
+    local n
+    local v
+    for n, v in pairs(node) do
+        local nnh = tostring(nh) .. "." .. tostring(n)
+
+        -- table
+        if (type(v) == 'table') then    
+            -- open one inner level {
+            mmident(level - 1)
+            io.write("/* " .. tostring(n)  ..  " */\n")
+            mmident(level - 1)
+            io.write("{\n")           
+            mmident(level)
+            io.write("lua_getglobal(L,\"" .. tostring(n) .. "\");\n")
+            mmident(level)
+            io.write("if (!lua_istable(L, -1)) {\n")
+            mmident(level + 1)
+            io.write("MM_ERR(\"" .. tostring(n) ..  " is not a table\");\n")
+            mmident(level + 1)
+            io.write("return -1;\n")
+            mmident(level)
+            io.write("}\n")
+             
+            mmgen_affectnodes(v,nnh, level + 1)
+            -- close one inner level} 
+            if(level > 0) then
+                mmident(level)
+                io.write("lua_pop(L,1);\t/* " .. nnh .. "*/\n")
+                mmident(level - 1)
+                io.write("}\n")
+            end
+        -- not table
+        else
+            mmident(level - 1)
+            if(level == 0) then
+                io.write("lua_getglobal(L,\"" .. tostring(n) .. "\");\n")
+            else
+                io.write("lua_getfield(L, -1, \"" .. tostring(n) .."\");\n")
+            end
+            mmident(level - 1)
+            -- numbers
+            if(v == 'int' or v == 'double' or v == 'float') then
+                io.write("if (!lua_isnumber(L, -1)) {\n")           
+                mmident(level)
+                io.write("MM_ERR(\"" .. nnh ..  " expected number\");\n")
+            end
+            -- strings
+            if(v == 'char*') then
+                io.write("if (!lua_isstring(L, -1)) {\n")           
+                mmident(level)
+                io.write("MM_ERR(\"" .. nnh ..  " expected string\");\n")
+            end
+            mmident(level)
+            io.write("return -1;\n")
+            mmident(level - 1)
+            io.write("}\n")
+
+            mmident(level - 1)
+            io.write ("cfg_->" .. tostring(nnh) ..  " = ");
+            -- numbers
+            if(v == 'int' or v == 'double' or v == 'float') then
+                io.write("(" .. v .. ")lua_tonumber(L,-1);\n");
+            end
+            -- strings
+            if(v == 'char*') then
+                io.write("strdup(lua_tostring(L,-1));\n");
+            end
+
+            mmident(level - 1)
+            io.write("lua_pop(L,1);\t/* ".. nnh .. " */\n")
+        end
+  end
+end
+
+function mmgen_affect(rootnode, structname)
+    -- header
+    io.write("static int mm_conf_load(struct " .. tostring(structname) .. "_s* cfg_, const char* f_)\n{\n")
+    io.write([[
+    lua_State *L = luaL_newstate();
+    luaopen_base(L);
+    if (luaL_loadfile(L, f_) || lua_pcall(L, 0, 0, 0)) {
+        MM_ERR("cannot run configuration file: %s",f_);
+        return -1
+    }
+]])
+    mmgen_affectnodes(rootnode, structname, 1)
+    -- footer
+    io.write("};\n")
+end
+
 
 -- [ main ] --------------------------------------------------------------------
 if not arg[1] then mmerr(mmerrs.missing_argument) end
@@ -73,3 +164,4 @@ if not mmapi_m.define then mmerr(mmerrs.missing_api_define) end
 if not mmapi then mmerr(mmerrs.missing_api) end
 
 mmgen_struct(mmapi, mmapi_m.basename)
+mmgen_affect(mmapi, mmapi_m.basename)
